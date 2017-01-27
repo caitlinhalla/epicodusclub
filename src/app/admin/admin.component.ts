170f8bf6c644873023d2294ca8f2599aaffa2ee5
Caitlin Ashtari <caitlinashtari@gmail.com>
2017-01-27 15:11:43 -0800

    add hack to reload admin page so that .touched method will refresh between form entry.

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -3,6 +3,8 @@ import { Student } from '../student.model';
 import { AngularFire, FirebaseObjectObservable } from 'angularfire2';
 import { StudentService } from '../student.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormsModule }  from '@angular/forms';
+import { Router } from '@angular/router';
 
 
 
@@ -15,10 +17,14 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class AdminComponent implements OnInit {
   form: FormGroup;
 
-  constructor(private studentService: StudentService, private fb: FormBuilder) {
+  constructor(private studentService: StudentService, private fb: FormBuilder, private router: Router) {
     this.form = fb.group({
-      'firstname' : [null, Validators.required],
-      'lastname' : [null, Validators.required]
+      'firstname' : ['', Validators.required],
+      'lastname' : ['', Validators.required],
+      'track' : ['', Validators.required],
+      'currentlanguage' : ['', Validators.required],
+      'currentlevel' : ['', Validators.pattern('^[0-9]+$')],
+      'treehousepoints' : ['', Validators.pattern('^[0-9]+$')]
     })
    }
 
@@ -28,6 +34,7 @@ export class AdminComponent implements OnInit {
   submitForm(firstname: string, lastname: string, track: string, currentlanguage: string, currentlevel: string, treehousepoints: number){
     var newStudent: Student = new Student(firstname, lastname, track, currentlanguage, currentlevel, treehousepoints);
     this.studentService.addStudent(newStudent);
+    window.location.reload(true);
   }
 
 }
